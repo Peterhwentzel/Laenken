c111c49d04d0f0f649022217f99fe3d0afd1eefc
Peter H Wentzel <Peterhwentzel@gmail.com>
2016-12-06 20:40:46 +0100

    style
    stylesheet

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,22 @@
-<div class="container">
+<div id="content_index" class="container">
 	<header>
-		<img src="images/logo.png" class="image" />
-		<span>Lænken</span>
+		<img src="images/logo.svg" class="image" />
+		<span>Lænke-ambulatorierne 
+		(Nordsjælland & Hovedstaden)</span>
 	</header>
 	<img src="images/img1.jpg" id="index-banner" />
 	<a href="?page=addict" class="btn btn-default btn-lg col-xs-12 btn-type1" style="margin-top: 5px;">Misbruger</a>
 	<a href="?page=relative" class="btn btn-default btn-lg col-xs-12 btn-type1" style="margin-top: 5px;">Pårørende</a>
 </div>
+<!--
+<footer id="footerSlide" data-show="false">
+	<div id="footerSlide-handle">
+		<span id="footerSlide-handle-icon-up" class="glyphicon glyphicon-menu-up"></span>
+		<span id="footerSlide-handle-icon-down" class="glyphicon glyphicon-menu-down"></span>
+	</div>
+	<div id="footerSlide-content">
+		<a href="tel:39455544"><span class="glyphicon glyphicon-earphone"</span>39455544</a> 
+		<a href="mailto:amager@laenke-ambulatorierne.dk" <span class="glyphicon glyphicon-envelope"</span>amager@laenke-ambulatorierne.dk</a>
+		<a href="http://www.laenken.dk/laenkens-ambulatorier-og-raadgivninger"<span class="glyphicon glyphicon-link"</span>>ambulatorium</a>
+	</div>
+</footer>-->
