6be0b51cb212be0a5a2e083104fe24b1b9c2f268
Peter H Wentzel <Peterhwentzel@gmail.com>
2017-01-11 22:06:33 +0100

    Update for some small changes
    Changes some padding and margin to balance the design in the mobile
    version

--- pages/index.php
+++ pages/index.php
@@ -5,7 +5,7 @@
 	</header>
 	<img src="images/img1.jpg" class="col-xs-12 col-sm-12 header-img" id="indexImg"/>
 	<div class="col-xs-12 col-sm-12">
-		<a href="?page=Patient" class="btn btn-default btn-lg col-xs-12 col-sm-6 btn-type1">Patient</a>
+		<a href="?page=Patient" class="btn btn-default btn-lg col-xs-12 col-sm-6 btn-type1" id="indexButton">Patient</a>
 		<a href="?page=Paarorende" class="btn btn-default btn-lg col-xs-12 col-sm-6 btn-type1" id="indexButton">Pårørende</a>
 	</div>
 </div> 
